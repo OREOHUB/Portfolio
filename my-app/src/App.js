b4e878f6981f0f684f5355d652de5c79e84d1649
Jona <210352@student.glu.nl>
2024-09-02 13:50:04 +0200

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
         Portfolio Jona Saija
+        </p>
+        <p>
+        CV
+        </p>
+        <p>
+        Instagram
+        </p>
+        <p>
+        LinkedIn
+        </p>
+        <p>
+        Discord
+        </p>
+
       </header>
     </div>
   );
